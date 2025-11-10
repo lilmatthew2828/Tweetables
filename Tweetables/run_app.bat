3e7c5f606e72c8f8eb3e64b1ca5ce74c1e1c25c1
daytonae <161365857+daytonae@users.noreply.github.com>
2025-11-09 19:13:52 -0500

    created run_app.bat
    created run_app.bat

--- Tweetables/run_app.bat
+++ Tweetables/run_app.bat
@@ -1,1 +1,18 @@
+REM run_app.bat: Tweetables Windows Launcher
+REM Purpose: Installs and launches application on macOS by creating VE, installing all packages, and running automatically
+REM Creator: Day Ekoi
+
+@echo off
+
+REM Step 1: Activate VE created during install
+
+    call venv\Scripts\activate
+
+REM Step 2: Launch Application
+
+    python Main.py
+
+REM Step 3: Keep window oopen so user can view output
+
+    pause
 
