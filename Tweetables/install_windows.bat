93cdb94bf7c422d37559d0c8664924f679c131b0
daytonae <161365857+daytonae@users.noreply.github.com>
2025-11-11 21:44:02 -0500

    Refactor Tweetables Windows installer script
    Updated installer script for clarity and flow.

--- Tweetables/install_windows.bat
+++ Tweetables/install_windows.bat
@@ -4,8 +4,30 @@ REM Creator: Day Ekoi
 
     @echo off
     echo ____________________________________
-    echo Tweetables Installer (Windows Only)
+    echo Tweetables Installer (Windows)
     echo ____________________________________
+
+
+REM Step 0: Check if venv already exists
+
+    if exist "venv\Scripts\activate" (
+        echo Virtual Environment already exists.
+        echo Skipping reinstallation...
+        call venv\Scripts\activate
+    if not exist ".env" (
+        echo No .env file found.
+        echo Make sure the .env file exists in the folder before launching.
+        pause
+        exit /b
+    )
+
+    echo Launching Tweetables...
+    python Main.py
+    echo.
+    echo Tweetables has finished running.
+    pause
+    exit /b
+)
 
 REM Step 1: Creating virtual environment 'venv'
 REM Keeps installed scripts isolated from system Python
@@ -27,17 +49,18 @@ REM Step 3: Install Project Dependencies
 
 REM Step 4: Verification of .env file
 
-    if not exist ".env" 
-    (    echo No .env file found
+    if not exist ".env" (    
+         echo No .env file found
          echo Make sure the .env file exists in the folder before launching.
          pause
          exit /b
     )
 
-REM Step 5: Installation Complete 
+REM Step 5: Installation Complete - Auto launch
 
     echo Installation Complete.
     echo Launching Tweetables...
     python Main.py
 
+    echo.
     pause
