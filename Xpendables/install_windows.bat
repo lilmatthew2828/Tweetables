f03fa6751308b88bcdd33d1e162c238a53ae3af4
daytonae <161365857+daytonae@users.noreply.github.com>
2025-12-03 20:40:11 -0500

    Disable Tweetables Windows Installer steps
    Comment out all installer steps for Windows.

--- Xpendables/install_windows.bat
+++ Xpendables/install_windows.bat
@@ -2,34 +2,39 @@ REM Tweetables Windows Installer
 REM Purpose: Creates virtual environement, installs required Python packes, and lauches the app with a double click
 REM Creator: Day Ekoi Fall 25'
 
-    @echo off
-    echo ____________________________________
-    echo Tweetables Installer (Windows Only)
-    echo ____________________________________
+
+  @echo off
+  cd /d "%~dp0"
+
+REM _____________________________________________________
+REM 
+REM    echo ____________________________________
+REM    echo Tweetables Installer (Windows Only)
+REM    echo ____________________________________
 
 REM Step 1: Creating virtual environment 'venv'
 REM Keeps installed scripts isolated from system Python
 
-    echo Please Wait, Creating Virtual Environment...
-    python -m venv venv
+REM   echo Please Wait, Creating Virtual Environment...
+REM   python -m venv venv
 
 REM Step 2: Activation of Virtual Environment
 REM 'call' required for Windows batch files to activate correctly
 
-    echo Activating Virtual Environment...
-    call venv\Scripts\activate
+REM    echo Activating Virtual Environment...
+REM    call venv\Scripts\activate
 
 REM Step 3: Install Project Dependencies
 
-    echo Installing Required Packages...
-    pip install --upgrade pip
-    pip install -r requirements.txt
+REM    echo Installing Required Packages...
+REM    pip install --upgrade pip
+REM    pip install -r requirements.txt
 
 REM Step 4: Installation Complete Twetables App is able to be launched (Main.py is the entry point)
 
-    echo Installation Complete.
-    echo Launching Tweetables...
-    python Main.py
+REM    echo Installation Complete.
+REM    echo Launching Tweetables...
+REM    python Main.py
 
 REM Step 5: Slight pause/delay so the command window doesnt immediately close and lets user read any output.
-    pause
+REM   pause
